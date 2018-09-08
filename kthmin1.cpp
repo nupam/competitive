b8c70a8d0ed0d5460ea3d1e17acc4e39d2a8e366
Anupam Kumar <31577441+nupam@users.noreply.github.com>
2018-09-08 22:13:56 +0530

    Update kthmin1.cpp

--- kthmin1.cpp
+++ kthmin1.cpp
@@ -1,67 +1,80 @@
-#include <iostream>
+#include <algorithm>
 #include <cmath>
-template <class T> inline void swap(T& a, T&b){
-T t = a;
-a = b;
-b = t;
-}
-int partition(int *b, int n, int k){
-int l = n - 1, p;
-for(int i=0; i<=l; i++) if(b[i]==k){p=i; break;}
-swap(b[l], b[p]);
-int pivot = b[l];
-int g = -1;
-for(int i=0; i<l; i++){
-if(b[i] < pivot && g != -1){
-swap(b[i],b[g]);
-g++;
-}
-else if(b[i]>=pivot && g == -1)
-g = i;
-}
-if(g!=-1) swap(b[g], b[l]);
-return g;
-}
+using namespace std;
+
 void insertionSort(int* a, int n){
-for(int i=1; i<n; i++){
-int j = i-1;
-while(j>=0 && a[j] > a[j+1]){
-swap(a[j], a[j+1]);
-j--;
+    if(n<=1 || !a) return;
+    for(int i=1; i<n; i++){
+        int t = a[i];
+        int j = i-1;
+        while(j>=0 && a[j] > t){
+            a[j+1] = a[j];
+            j--;
+        }
+        a[j+1] = t;
+    }
 }
+
+int partition(int* a, int n){
+    if(n==1) return 0;
+    int p = a[--n];
+    int b = -1;
+    for(int i=0; i<n; i++){
+        if(a[i] >= p);
+        else swap(a[++b], a[i]);
+    }
+    swap(a[++b], a[n]);
+    return b;
 }
+
+int findMedianOfMedians(int* a, int n){
+    int* b = new int[n];
+    for(int i=0; i<n; i++) b[i] = a[i];
+    while(n>1){
+        int g = ceil(1.0*n/5);
+        bool extra =( n%5!=0);
+        int* medians = new int[g];
+        if(extra) g--;
+        for(int i=0; i<g; i++){
+            insertionSort(b+5*i, 5);
+            medians[i] = b[5*i+2];
+        }
+        if(extra){
+            int eit = n-5*g;
+            insertionSort(b+5*g, eit);
+            medians[g] = b[5*g + ((int)ceil(1.0*eit/2)-1)];
+        }
+        n = g;
+        if(extra) n++;
+        delete[] b;
+        b = medians;
+    }
+    int ans = b[0];
+    delete[] b;
+    return ans;
 }
-int kthMin(int *a, int n, int k){
-if(n==1) return a[0];
-int* median = new int[n], l = n;
-for(int i=0; i<n; i++) median[i] = a[i];
-while(l>1){
-int gp = ceil(1.0*l/5);int* temp = new int[gp];
-int it_count = l%5==0 ? gp:gp-1;
-for(int i=0; i<it_count; i++){
-insertionSort(median+5*i, 5);
-temp[i] = (median+5*i)[2];
+
+int kthmin(int* a, int n, int k){
+    if(n==1) return a[0];
+    int m = findMedianOfMedians(a, n);
+    int p = (int)(find(a, a+n, m)-a);
+    swap(a[p], a[n-1]);
+    p = partition(a, n);
+    if(p+1==k) return a[p];
+    else if(p>=k) return kthmin(a, p, k);
+    else return kthmin(a+p+1, n-p-1, k-p-1);
 }
-if(l%5 !=0){
-insertionSort(a+(gp-1)*5, l%5);
-temp[gp-1] = (a+(gp-1)*5)[(int)ceil((l%5)/2) -1];
+
+
+int main() {
+	int t; cin >> t;
+	while(t--){
+	    int n, k; cin >> n;
+	    int *a = new int[n];
+	    for(int i=0; i<n; i++) cin >> a[i];
+	    cin >> k;
+	    cout << kthmin(a, n, k) <<  '\n';
+	    delete[] a;
+	}
+	return 0;
 }
-delete[] median;
-median = temp;
-l = gp;
-}
-int q = partition(a, n, median[0]);
-if(q+1 == k) return a[q];
-if(q+1 < k) return kthMin(a+q+1, n-q-1, k-q-1);
-return kthMin(a, q, k);
-}
-int main(){
-int n,k;
-std::cout << "Number of Elements: "; std::cin >> n;
-int* a = new int[n];
-for(int i=0; i<n; i++){
-std::cin >> a[i];
-}
-std::cout << "K: "; std::cin >> k;
-std:: cout << kthMin(a, n, k) << "\n";
-//for(int i=0; i<n; i++) std::cout << a[i] << ' ';
